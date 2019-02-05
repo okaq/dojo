b41f19b5806b3b77ceeab067042d3b4732294e22
AQ <aq@okaq.com>
2019-02-05 11:03:21 -0500

    Start web server

--- cuto.go
+++ cuto.go
@@ -4,4 +4,30 @@
 // 2019-02-06
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "cuto.html"
+)
+
+func CutoHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func motd() {
+	fmt.Println("okaq web serv on localhost:8080")
+	fmt.Printf("start: %s\n", time.Now().String())
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", CutoHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
 
